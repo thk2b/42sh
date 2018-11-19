459fc85e67c5287f98297ce99ab0c48c06c7400e
thk2b <theokobb@gmail.com>
2018-11-19 08:49:11 -0800

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 19:15:39 by tkobb             #+#    #+#             */
-/*   Updated: 2018/11/18 20:26:49 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/11/19 08:48:43 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,18 @@
 **		Execute
 **	Teardown
 */
+
+static int	init_shell(void)
+{
+	if (path_init())
+		return (1);
+	return (0);
+}
+
+static void	teardown_shell(void)
+{
+	path_teardown();
+}
 
 static int	process_command(void)
 {
