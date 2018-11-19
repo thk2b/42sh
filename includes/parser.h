6626d68854ca6eb496e0d6c88066715fa5f91f6e
thk2b <theokobb@gmail.com>
2018-11-18 19:48:35 -0800

    add parser to srcs/parser and parser/includes to includes

--- includes/parser.h
+++ includes/parser.h
@@ -3,19 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ale-goff <ale-goff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 16:31:26 by tkobb             #+#    #+#             */
-/*   Updated: 2018/11/17 16:48:57 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/11/18 18:47:59 by dmendelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <strings.h>
+#include <string.h>
+#include <libft.h>
+#include "parser.h"
+
+# define IS_SPACE(x) (x == ' ' || x == '\t' || x == '\n')
+# define IS_OP(x) (x == '&' || x == '|')
+# define WOW() printf("->%s\n", __func__)
 # define REDIRECT_LEFT			1
 # define REDIRECT_RIGHT			2
 # define REDIRECT_APPEND_RIGHT	3
+
+# define SEEKING_END 1
+# define END 2
 
 typedef struct					s_redirect
 {
@@ -25,33 +40,60 @@ typedef struct					s_redirect
 	struct s_redirect			*next;
 }								t_redirect;
 
-typedef struct					s_assign
-{
-	char			*value;
-	struct s_assign	*next;
-}								t_assign;
-
 # define T_CMD					1
 # define T_PIPE					2
 # define T_AND					3
 # define T_OR					4
 # define T_SEMI					5
+# define T_ALPHANUM				6
+# define T_QUOTE				7
+# define T_BRACKET				8
+# define T_PAREN				9
+# define T_BACKTICK				10
+# define T_SPACE				11
+# define T_COMMENT				12
 
-typedef struct					s_cmd
+typedef struct			s_nodes
 {
-	t_redirect					*redirects;
-	t_assign					*assign;
-	char						**argv;
-	int							return_status;
-}								t_cmd;
+	char				*content;
+	struct s_nodes		*next;
+}						t_nodes;
+
+typedef struct			s_list
+{
+	t_nodes				*head;
+	t_nodes				*tail;
+}						t_list;
+
+/*typedef struct					s_list
+{
+	char						*content;
+	struct s_list				*next;
+}
+t_list;
+*/
 
 typedef struct					s_tree
 {
 	char						type;
 	struct s_tree				*left;
 	struct s_tree				*right;
-	struct s_tree				*parent;
-	struct s_cmd				*data;
+	struct s_list				*args;
+	//	struct s_cmd				*data;
 }								t_tree;
 
+typedef struct					s_cmd
+{
+	t_redirect					*redirects;
+	char						**argv;
+	int							return_status;
+}								t_cmd;
+
+typedef struct					s_token
+{
+	int							type;
+	int							status;
+	char						*content;
+}								t_token;
+void			read_line(void);
 #endif
