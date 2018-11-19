fb003ccb923cef5a2642eca1c117d083f21e5dca
thk2b <theokobb@gmail.com>
2018-11-19 08:45:05 -0800

    init redirects. only find last redirect and heredoc

--- srcs/exec/redirects.c
+++ srcs/exec/redirects.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 07:31:07 by tkobb             #+#    #+#             */
-/*   Updated: 2018/11/19 08:08:38 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/11/19 08:43:35 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@
 **	the file becomes stdout
 */
 
-static int	init_redirect(t_redirect *redirect, int append)
+static int			init_redirect(t_redirect *redirect, int append)
 {
 	int		fd;
 	int		flags;
 
-	flags = O_CREAT & append ? O_APPEND : O_TRUNC;
+	flags = O_WRONLY & O_CREAT & append ? O_APPEND : O_TRUNC;
 	if ((fd = open(redirect->path, flags, CREAT_PERMS)) == -1)
 		return (error("open"));
-	if (dup2(fd, 1) == -1)
+	if (dup2(fd, redirect->fd) == -1)
 		return (error("dup2"));
 	return (0);
 }
@@ -34,30 +34,46 @@ static int	init_redirect(t_redirect *redirect, int append)
 **	the file becomes stdin
 */
 
-static int	init_heredoc(t_redirect *redirect)
+static int			init_heredoc(t_redirect *redirect)
 {
-	
+	int	fd;
+
+	if ((fd = open(redirect->path, O_RDONLY)) == -1)
+		return (error(redirect->path));
+	if (dup2(fd, 0) == -1)
+		return (error("dup2"));
+	return (0);
 }
 
-int			init_redirects(t_redirect *redirects)
+static void			get_last_redirects(t_redirect *redirects,
+	t_redirect **last_redirect, t_redirect **last_heredoc)
 {
-	char		did_heredoc;
-	char		did_redirect;
 	t_redirect	*r;
+	t_redirect	*lr;
+	t_redirect	*lh;
 
 	r = redirects;
+	lr= NULL;
+	lh = NULL;
 	while (r)
 	{
-		if (!did_redirect && (r->type & REDIRECT_RIGHT)
-		&& (did_redirect = 1) == 1) // NO! do the last redirection only
-			init_redirect(r, 0);
-		else if (!did_redirect && (r->type & REDIRECT_APPEND_RIGHT)
-		&& (did_redirect = 1) == 1)
-			init_redirect(r, 1);
-		else if (!did_heredoc && (r->type & REDIRECT_LEFT)
-		&& (did_heredoc = 1) == 1)
-			init_heredoc(r);
+		if (r->type == REDIRECT_APPEND_RIGHT || r->type == REDIRECT_RIGHT)
+			lr = r;
+		else if (r->type == REDIRECT_LEFT)
+			lh = r;
 		r = r->next;
 	}
+	*last_redirect = lr;
+	*last_heredoc = lh;
+}
+
+int					init_redirects(t_redirect *redirects)
+{
+	t_redirect	*redirect;
+	t_redirect	*heredoc;
+
+	get_last_redirects(redirects, &redirect, &heredoc);
+	init_redirect(redirect, redirect->type == REDIRECT_APPEND_RIGHT);
+	init_heredoc(heredoc);
 	return (0);
 }
