2d8974a0f0d79f552197b1941a2b07b87f0e5b9d
Anton Cakste <acakste@e1z1r6p15.42.us.org>
2018-11-19 10:16:52 -0800

    local var now has set, rm, get value

---
+++ srcs/local_var/local_var.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   local_var.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acakste <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/19 08:26:55 by acakste           #+#    #+#             */
+/*   Updated: 2018/11/19 08:26:57 by acakste          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_sh.h>
+
+static t_var	*g_var[NUM_SLOTS] = {
+	 NULL
+};
+
+
+t_var	*find_lst(char *key)
+{
+	unsigned long	hash;
+	t_var			*cur;
+
+	if (!key)
+		return (NULL);
+	hash = hash_var(key);
+	cur = g_var[hash % NUM_SLOTS];
+	return (cur);
+}
+
+t_var	*create_var_elem(char *key, char *value)
+{
+	t_var	*new;
+
+	new = (t_var*)malloc(sizeof(t_var));
+	new->key = ft_strdup(key);
+	new->value = ft_strdup(value);
+	new->next = NULL;
+	return (new);
+}
+
+char	*get_local_var(char *key)
+{
+	t_var			*cur;
+
+	cur = find_lst(key);
+	while (cur && ft_strcmp(cur->key, key))
+		cur = cur->next;
+	return ((cur) ? cur->value : NULL); // return a copy of the variable?
+}
+
+t_var	*rm_local_var_inst(t_var *cur, char *key)
+{
+	t_var	*tmp;
+
+	if (!cur)
+		return (NULL);
+	if (ft_strcmp(cur->key, key) == 0)
+	{
+		tmp = cur->next;
+		free(cur->value);
+		free(cur->key);
+		free(cur);
+		return (tmp);
+	}
+	cur->next = rm_local_var_inst(cur->next, key);
+	return (cur);
+}
+
+int		rm_local_var(char *key)
+{
+	unsigned long	hash;
+	t_var			*slot;
+
+	hash = hash_var(key);
+	slot = g_var[hash % NUM_SLOTS];
+	slot = rm_local_var_inst(slot, key);
+	g_var[hash % NUM_SLOTS] = slot;
+	return (0);
+}
+
+int		store_local_var(char *key, char *value)
+{
+	t_var	*cur;
+	t_var	*new;
+	unsigned long	hash;
+
+	if (!key)
+		return (1);
+	hash = hash_var(key);
+	cur = g_var[hash % NUM_SLOTS];
+	while (cur && ft_strcmp(cur->key, key))
+		cur = cur->next;
+	if (cur)
+	{
+		free(cur->value);
+		cur->value = ft_strdup(value);
+	}
+	else
+	{
+		new = create_var_elem(key, value);
+		new->next = g_var[hash % NUM_SLOTS];
+		g_var[hash % NUM_SLOTS] = new;
+	}
+	return (0);
+}
+
+/*
+**	djb2 by Dan Bernstein
+*/
+unsigned long	hash_var(char *key)
+{
+	unsigned long hash = 5381;
+    int c;
+
+    while ((c = *key++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	return (hash);
+}
---
+++ srcs/local_var/main.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acakste <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/19 09:06:24 by acakste           #+#    #+#             */
+/*   Updated: 2018/11/19 09:06:25 by acakste          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_sh.h>
+#include <stdio.h>
+
+int		main(void)
+{
+	// char *key = "foo";
+	// char *value = "bar";
+
+	// store_local_var(key, value);
+	// value = "baz";
+	// store_local_var(key, value);
+	// rm_local_var(key);
+	// printf("Result: %s\n", get_local_var(key));
+
+	char	arr[5] = {'\0'};
+	int	i;
+	int	j;
+	char *key2 = "ag";
+	unsigned long hash;
+
+	hash = hash_var(key2);
+	i = 0;
+	while (i < 25)
+	{
+		j = 0;
+		arr[0] = i + 'a';
+		while (j < 25)
+		{
+			arr[1] = j + 'a';
+			if (hash_var(arr) % 20 == hash % 20)
+			{
+				(void)j;
+				printf("%s\n", arr);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	char	*keys[10] = {"ag", "bn", "ca", "cu", "dh", "eo", "fb", "fv", "gi", "hp"};
+	char	value[2] = {'\0'};
+
+	int k = 0;
+	while (k < 10)
+	{
+		value[0] = k + 48;
+		store_local_var(keys[k], value);
+		k++;
+	}
+
+	int k2 = 1;
+
+	while (k2 < 10)
+	{
+		k = 0;
+		while (k < 10)
+		{
+			printf("num: %d  %s\n", k, get_local_var(keys[k]));
+			k++;
+		}
+		rm_local_var(keys[k2]);
+		k2 += 2;
+	}
+	return (0);
+}
