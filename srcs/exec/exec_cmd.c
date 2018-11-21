e22032d1e7e359d57df58308553a7be5bae1d93a
Anton Cakste <acakste@e1z1r6p15.42.us.org>
2018-11-21 10:45:09 -0800

    moved expand to after redirects

--- srcs/exec/exec_cmd.c
+++ srcs/exec/exec_cmd.c
@@ -38,10 +38,10 @@ int		exec_cmd(t_tree *tree)
 
 	if (tree->data->assign)
 		store_assignments(tree->data->assign);
-	expand(tree->data->argv);
 	if (tree->data->redirects)
 		init_redirects(tree->data->redirects);
 	return_status = 0;
+	expand(tree->data->argv);
 	if (builtin(tree->data->argv, &return_status) == 0) // if we execute builtin, stop
 		return (return_status);
 	if ((path = search_cmd(tree->data->argv[0])) == NULL)
