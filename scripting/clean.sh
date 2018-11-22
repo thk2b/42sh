2682d344647249910f493cdc0565fc8765b592b5
DavidMendelovits <David.Mendelovits@gmail.com>
2018-11-21 19:50:01 -0800

    shell scripts for testing done-ish. more to come soon

---
+++ scripting/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm -rf shell
+rm -rf ft_shell
+rm test_input.*
+
--- scripting/test.sh
+++ scripting/test.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-./Simple-Fuzzer/sfuzz -f -q -O -n Simple-Fuzzer/configs/literals file -L testfile
+./Simple-Fuzzer/sfuzz -f -q -O -n Simple-Fuzzer/configs/literals -L test_input
+mkdir shell
+mkdir ft_shell
 
-for x in testfile.*
+for x in test_input.*
 do
-    cat $x | sh > sh_output.$x
+    cat $x | sh > shell/$x
+	cat $x | ../42sh > ft_shell/$x
+	if diff ft_shell/$x shell/$x
+	then
+		echo "same!"
+	else
+		echo "found error!"
+		break
+	fi 
 done
