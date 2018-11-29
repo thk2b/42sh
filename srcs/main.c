63ee894913f6d16708862430dbb7425d7c4e4a02
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2018-11-29 12:27:53 -0800

    Added history substitution

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 19:15:39 by tkobb             #+#    #+#             */
-/*   Updated: 2018/11/22 20:32:28 by dmendelo         ###   ########.fr       */
+/*   Updated: 2018/11/29 12:25:00 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,6 +93,7 @@ static int	process_command(int *status)
 
 	line = NULL;
 	line = ft_readline("\x1b[1;37m""42sh:""\x1b[0m"" $> ", 9, RL_DEFAULT);
+	line = history_substitution(line);
 	if (!line)
 		return (0);
 	root = parse(line);
