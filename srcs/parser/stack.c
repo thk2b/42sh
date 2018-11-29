e1a9ca8133aeeaad86626e837420fc864ebf71f0
Anton Cakste <acakste@e1z1r6p15.42.us.org>
2018-11-28 19:44:24 -0800

    stack normed

--- srcs/parser/stack.c
+++ srcs/parser/stack.c
@@ -12,39 +12,39 @@
 
 #include <stack2.h>
 
-int		pop(t_node **top)
+int			pop(t_node **top)
 {
-    t_node  *tmp;
-    int		content;
-    
-    content = 0; // could there ever be a problem where pop returns 0 it stack is empty?
-    tmp = *top;
-    if (*top)
-    {
-        content = (*top)->content;
-        *top = (*top)->next;
-        free(tmp);
-    }
-    return (content);
+	t_node	*tmp;
+	int		content;
+
+	content = 0; // could there ever be a problem where pop returns 0 it stack is empty?
+	tmp = *top;
+	if (*top)
+	{
+		content = (*top)->content;
+		*top = (*top)->next;
+		free(tmp);
+	}
+	return (content);
 }
 
-void push(t_node **top, int content)
+void		push(t_node **top, int content)
 {
-    t_node *node;
-    
-    node = (t_node*)malloc(sizeof(t_node));
-    node->content = content;
-    node->next = *top;
-    *top = node;
-    return ;
+	t_node *node;
+
+	node = (t_node*)malloc(sizeof(t_node));
+	node->content = content;
+	node->next = *top;
+	*top = node;
+	return ;
 }
 
 int			is_empty(t_node *top)
 {
-    return (top == NULL);
+	return (top == NULL);
 }
 
 int			peek(t_node *top)
 {
-    return ((top) ? top->content : 0);
+	return ((top) ? top->content : 0);
 }
