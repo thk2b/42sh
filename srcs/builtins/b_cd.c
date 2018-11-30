b3b076d46255c3deff03c08d2a97058397b795bf
Anton Cakste <acakste@e1z1r6p15.42.us.org>
2018-11-30 06:36:34 -0800

    fixed issue with cd

--- srcs/builtins/b_cd.c
+++ srcs/builtins/b_cd.c
@@ -13,20 +13,20 @@
 #include <ft_sh.h>
 #include <env.h>
 
-static int	special_cases(char *dst)
+static int	special_cases(char **dst)
 {
-	if (dst == NULL)
+	if (*dst == NULL)
 	{
-		MCK(dst = ft_getenv("HOME"), 1);
+		MCK(*dst = ft_getenv("HOME"), 1);
 	}
-	else if (dst[0] == '-')
+	else if ((*dst)[0] == '-')
 	{
-		if ((dst = ft_getenv("OLDPWD")) == NULL)
+		if (((*dst) = ft_getenv("OLDPWD")) == NULL)
 		{
 			ft_dprintf(2, "%s: OLDPWD not set\n", "42sh");
 			return (1);
 		}
-		ft_printf("%s\n", dst);
+		ft_printf("%s\n", (*dst));
 	}
 	return (0);
 }
@@ -39,7 +39,7 @@ int			b_cd(char **av)
 	if (av == NULL || av[0] == NULL)
 		return (1);
 	dst = av[1];
-	if (special_cases(dst))
+	if (special_cases(&dst))
 		return (1);
 	MCK(curpwd = ft_getenv("PWD"), 1);
 	if (ft_setenv("OLDPWD", curpwd, 1))
