cb210c25286939d7f4a3d67e17e14ca99235a31e
Alexandre Le goff <ale-goff@student.42.us.org>
2018-12-01 17:48:41 -0800

    aggreg does not work yet

--- srcs/parser/parse_tokens.c
+++ srcs/parser/parse_tokens.c
@@ -6,7 +6,7 @@
 /*   By: dmendelo <dmendelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 10:18:05 by dmendelo          #+#    #+#             */
-/*   Updated: 2018/12/01 13:10:16 by ale-goff         ###   ########.fr       */
+/*   Updated: 2018/12/01 17:48:08 by ale-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,13 @@ int						append_word_argv(char *word, t_cmd **cmd)
 static void				handle_word(t_nodes *traverse, int aggreg,
 									t_nodes *prev, t_cmd **command)
 {
-	if (traverse->next && is_red(traverse->next->content))
-		;
+	if (aggreg)
+	{
+		if (traverse->next && is_red(traverse->next->content))
+			;
 	else if (prev && ft_strequ(prev->content, "&") && aggreg)
-		;
+			;
+	}
 	else
 		append_word_argv(traverse->content, command);
 }
