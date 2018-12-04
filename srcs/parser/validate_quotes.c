f6b543d7f7b21a30030674892aaeb958b76f7814
Alexandre Le goff <ale-goff@student.42.us.org>
2018-12-03 19:22:40 -0800

    increase the index

--- srcs/parser/validate_quotes.c
+++ srcs/parser/validate_quotes.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:22:00 by tkobb             #+#    #+#             */
-/*   Updated: 2018/12/03 19:16:01 by ale-goff         ###   ########.fr       */
+/*   Updated: 2018/12/03 19:21:26 by ale-goff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,23 @@ int					validate_quotes(char *input)
 
 	i = -1;
 	stack = NULL;
-	empty = 0;
 	while (input[++i])
 	{
 		if (input[i] == '`')
 		{
-			if (i > 0 && input[i - 1] == '\\')
+			if (i > 0 && input[i - 1] == '\\' && IS_QU(input[i]))
 				continue ;
 			else
-				while (input[i] != '`')
+				while (input[++i] != '`')
 				{
 					if (IS_QU(input[i]) && input[i] != '\\' && input[i] != '`')
 						push(&stack, input[i]);
-					else if (IS_QU(input[i]) && peek(stack) == input[i])
+					else if (IS_QU(input[i]) && peek(stack) == input[i] && input[i] != '`')
 						pop(&stack);
 				}
 		}
 	}
-	if (is_empty(stack))
-		empty = 1;
+	empty = is_empty(stack) ? 1 : 0;
 	free(stack);
 	return (empty);
 }
